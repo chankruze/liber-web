42c87a66c85e17f97df0db9b89a3999ef5ee6dd2
chankruze <chankruze@gmail.com>
2024-01-22 11:05:15 +0530

    style(tailwind): install and setup tailwind css
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ app/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = `Liber`;
+export const SITE_DESCRIPTION = `Manage all Your Links at One Place!`;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,14 @@
 import type { MetaFunction } from "@remix-run/node";
+import { SITE_DESCRIPTION, SITE_TITLE } from "~/consts";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: `${SITE_TITLE} - ${SITE_DESCRIPTION}` },
+    { name: "og:title", content: SITE_TITLE },
+    { name: "description", content: SITE_DESCRIPTION },
   ];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  return <main></main>;
 }
